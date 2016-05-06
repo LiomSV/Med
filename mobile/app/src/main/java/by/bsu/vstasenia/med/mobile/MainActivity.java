47b8cf63caa57f26b01a3943364e09c5fe62f0be
LiomSV <liomsv@gmail.com>
2016-05-06 10:31:14 +0300

    add buttons changing status into mobile app

--- mobile/app/src/main/java/by/bsu/vstasenia/med/mobile/MainActivity.java
+++ mobile/app/src/main/java/by/bsu/vstasenia/med/mobile/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -18,24 +20,30 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String UPDATE_TIMER_NAME = "updateTimer";
     private Location currentLocation = null;
+    private Timer timer;
+    private int statusOrd = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                .enableAutoManage(this, null)
-//                .addApi(Api)
 
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -64,13 +72,21 @@ public class MainActivity extends AppCompatActivity {
             };
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, listener);
+
+            timer = new Timer(UPDATE_TIMER_NAME);
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                        update();
+                }
+            }, 0, 3000);
         }
     }
 
     public void onAddCall(View view) {
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        String url = "http://192.168.0.41:8080/rest/call/add_coor";
+        String url = getUrl("rest/call/add_coor");
         StringRequest request = new StringRequest(Request.Method.POST, url, new Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -97,6 +113,26 @@ public class MainActivity extends AppCompatActivity {
         queue.add(request);
     }
 
+    public void onFree(View view) {
+        statusOrd = 0;
+    }
+
+    public void onMoving(View view) {
+        statusOrd = 1;
+    }
+
+    public void onTreatment(View view) {
+        statusOrd = 2;
+    }
+
+    public void onTransporting(View view) {
+        statusOrd = 3;
+    }
+
+    public void onBreak(View view) {
+        statusOrd = 4;
+    }
+
     private boolean isNewLocationBetter(Location newLoc) {
         return currentLocation == null || (newLoc.getTime() - currentLocation.getTime() > 1000);
     }
@@ -105,5 +141,72 @@ public class MainActivity extends AppCompatActivity {
         this.currentLocation = newLoc;
         TextView textCoord = (TextView) findViewById(R.id.textCoord);
         textCoord.setText(String.format("Coord: %s; %s", currentLocation.getLatitude(), currentLocation.getLongitude()));
+    }
+
+    private String getUrl(String suffix) {
+        return ((EditText) findViewById(R.id.fldPrefix)).getText() + suffix;
+    }
+
+    private void update() {
+        String numerStr = ((EditText) findViewById(R.id.fldNumber)).getText().toString();
+        if (numerStr != null && !numerStr.isEmpty()) {
+            String url = getUrl("rest/crew/update_status");
+            Map<String, String> params = new HashMap<>();
+            params.put("crewId", numerStr);
+            params.put("status", Integer.toString(statusOrd));
+            if (currentLocation != null) {
+                params.put("lat", Double.toString(currentLocation.getLatitude()));
+                params.put("lng", Double.toString(currentLocation.getLongitude()));
+            }
+            Request<JSONObject> request = new CustomRequest(Request.Method.POST, url, params, new Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    ((TextView) findViewById(R.id.textMain)).setText(response.toString());
+                    try {
+                        String status = response.getString("status");
+                        if (status.equals("FREE")) {
+                            ((TextView) findViewById(R.id.textStatus)).setText("Статус: свободен");
+                            ((Button) findViewById(R.id.btnMoving)).setEnabled(true);
+                            ((Button) findViewById(R.id.btnTreatment)).setEnabled(false);
+                            ((Button) findViewById(R.id.btnTransportation)).setEnabled(false);
+                            ((Button) findViewById(R.id.btnFree)).setEnabled(false);
+                        } else if (status.equals("MOVING")) {
+                            ((TextView) findViewById(R.id.textStatus)).setText("Статус: в пути");
+                            ((Button) findViewById(R.id.btnMoving)).setEnabled(false);
+                            ((Button) findViewById(R.id.btnTreatment)).setEnabled(true);
+                            ((Button) findViewById(R.id.btnTransportation)).setEnabled(false);
+                            ((Button) findViewById(R.id.btnFree)).setEnabled(false);
+                        } else if (status.equals("TREATMENT")) {
+                            ((TextView) findViewById(R.id.textStatus)).setText("Статус: на месте");
+                            ((Button) findViewById(R.id.btnMoving)).setEnabled(false);
+                            ((Button) findViewById(R.id.btnTreatment)).setEnabled(false);
+                            ((Button) findViewById(R.id.btnTransportation)).setEnabled(true);
+                            ((Button) findViewById(R.id.btnFree)).setEnabled(true);
+                        } else if (status.equals("TRANSPORTING")) {
+                            ((TextView) findViewById(R.id.textStatus)).setText("Статус: транспортировка");
+                            ((Button) findViewById(R.id.btnMoving)).setEnabled(false);
+                            ((Button) findViewById(R.id.btnTreatment)).setEnabled(false);
+                            ((Button) findViewById(R.id.btnTransportation)).setEnabled(false);
+                            ((Button) findViewById(R.id.btnFree)).setEnabled(true);
+                        } else if (status.equals("BREAK")) {
+                            ((TextView) findViewById(R.id.textStatus)).setText("Статус: занят");
+                            ((Button) findViewById(R.id.btnMoving)).setEnabled(false);
+                            ((Button) findViewById(R.id.btnTreatment)).setEnabled(false);
+                            ((Button) findViewById(R.id.btnTransportation)).setEnabled(false);
+                            ((Button) findViewById(R.id.btnFree)).setEnabled(true);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    ((TextView) findViewById(R.id.textMain)).setText("Ops... error");
+                }
+            });
+
+            Volley.newRequestQueue(MainActivity.this).add(request);
+        }
     }
 }
